5a5d3b6b4a4350c51ac4da334863a971863e298c
Neo-yoyo <bluecloud9608@gmail.com>
2022-12-05 09:45:31 +1000

    Пишем в файл

--- scr/vector_random_generator/main.cpp
+++ scr/vector_random_generator/main.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
+#include <fstream>
 
 #include <vector_random_generator/vector_random_generator.hpp>
 
-
-//    std::vector<std::uint16_t> vector;
-//    std::vector<std::uint32_t> vector;
-//    std::vector<std::int64_t> vector;
-
-//    std::vector<std::int8_t> vector;
-//    std::vector<std::int16_t> vector;
-//    std::vector<std::int32_t> vector;
-//    std::vector<std::int64_t> vector;
-
-//    std::vector<std::string> vector;
-
-//    std::vector<float> vector;
-//    std::vector<double> vector;
-
-//    std::vector<xt::dec8_t> vector;
-//    std::vector<xt::udec8_t> vector;
 
 template<typename Type>
 void vector_type (std::uint64_t length = 200) {
@@ -45,14 +29,52 @@ void vector_type (std::uint64_t length = 200) {
 
     std::cout << "}" << std::endl;
 
+
+    /// Всё что вывели в консоль - записываем в файл
+    /// выходная директория
+    std::string path_out{"/home/neo_yoyo/Документы/trash"};
+
+    /// формируем имя выходного файла
+    std::string out_filename = path_out + "/" + "random_set.txt";
+
+    /// открываем выходной файл
+    std::ofstream out_file(out_filename);
+
+    /// Если мы не можем открыть этот файл для записи данных,
+    if (!out_file)
+    {
+        /// то выводим сообщение об ошибке и выполняем функцию exit()
+        std::cerr << "File could not be opened for writing!" << std::endl;
+        exit(1);
+    }
+
+    out_file << "{";
+    for (std::size_t i=0; i < length; i++){
+        out_file << vector[i];
+
+        if(i != (length - 1))
+            out_file << ", ";
+
+        if((i + 1) % 10 == 0 && i != (length - 1))
+            out_file << std::endl;
+
+    }
+
+    out_file << "}" << std::endl;
+
+    /// закрываем файл
+    out_file.close();
+
 }
+
+
 
 int main(int argc, char* argv[]) {
 
     if(argc == 1) {
         std::cout << "run" << std::endl;
 
-        vector_type<std::int8_t>();
+        vector_type<std::uint16_t>();
     }
 
     else if(argc == 2){
@@ -60,7 +82,7 @@ int main(int argc, char* argv[]) {
 
         std::uint64_t length = std::stol(argv[1]);
 
-        vector_type<std::int8_t>(length);
+        vector_type<std::uint16_t>(length);
     }
 
     else if(argc == 3){
@@ -69,8 +91,29 @@ int main(int argc, char* argv[]) {
         std::string type(argv[1]);
         std::uint64_t length = std::stol(argv[2]);
 
-        if(type == "uint8_t"){
+        if(type == "uint8"){
             vector_type<std::uint8_t>(length);
+        }
+        else if(type == "uint16"){
+           vector_type<std::uint16_t>(length);
+        }
+        else if(type == "uint32"){
+           vector_type<std::uint32_t>(length);
+        }
+        else if(type == "uin64"){
+           vector_type<std::uint64_t>(length);
+        }
+        else if(type == "int8"){
+           vector_type<std::int8_t>(length);
+        }
+        else if(type == "int16"){
+           vector_type<std::int16_t>(length);
+        }
+        else if(type == "int32"){
+           vector_type<std::int32_t>(length);
+        }
+        else if(type == "int64"){
+           vector_type<std::int64_t>(length);
         }
         else if(type == "string"){
            vector_type<std::string>(length);
@@ -78,5 +121,9 @@ int main(int argc, char* argv[]) {
         else if(type == "float"){
             vector_type<float>(length);
         }
+        else if(type == "double"){
+            vector_type<double>(length);
+        }
     }
+
 }

d654c96a4f9fed1cde7f90d93f9688a8ba439396
Neo-yoyo <bluecloud9608@gmail.com>
2023-04-07 15:00:44 +1000

    исправления

--- scr/vector_random_generator/main.cpp
+++ scr/vector_random_generator/main.cpp
@@ -133,24 +133,6 @@ int main(int argc, char* argv[]) {
 }
 
 
-/// пример
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> fb9db57 (amend 2)
-
-=======
-/// amend
->>>>>>> 89be182 (amend)
-<<<<<<< HEAD
-=======
-
->>>>>>> 6d77136 (amend)
-=======
->>>>>>> fb9db57 (amend 2)
-
 
 
 
